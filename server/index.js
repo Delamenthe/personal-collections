27a751829c83f1649795d9c8c1d7a704ac46c72c
Арина <arinych24@gmail.com>
2022-01-25 23:52:30 +0300

    error handling

--- server/index.js
+++ server/index.js
@@ -4,11 +4,19 @@ const db = require('../server/db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+
+
 const PORT =process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+
+
+app.use(errorHandler)
+
+
  start = async () => {
     try{
         app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))

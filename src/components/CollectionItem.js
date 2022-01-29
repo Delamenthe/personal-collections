9c3ec534ff52941bc1c4a9396d499a6b7d09245b
“Delamenthe” <arinych24@gmail.com>
2022-01-29 19:47:16 +0300

    add images from cloud

--- src/components/CollectionItem.js
+++ src/components/CollectionItem.js
@@ -6,9 +6,9 @@ import {COLLECTIONS_ROUTE} from "../utils/consts";
 const CollectionItem = ({collection}) => {
     const navigate = useNavigate();
     return (
-        <Col md={3} className={"mt-3"} onClick={()=>navigate(COLLECTIONS_ROUTE+'/'+collection.id)}>
+        <Col md={3} className={"mt-3"} onClick={()=>navigate(COLLECTIONS_ROUTE+collection.id)}>
             <Card style={{width: 300, cursor: "pointer"}} border={"light"}>
-                <Image width={300} height={300} src={''+process.env.REACT_APP_API_URL+ collection.img}/>
+                <Image width={300} height={300} src={'https://res.cloudinary.com/delamenthe/image/upload/v1643469481/'+collection.img+'.jpg'}/>
                 <h4>{collection.name}</h4>
                 <div>{collection.description}</div>
             </Card>
